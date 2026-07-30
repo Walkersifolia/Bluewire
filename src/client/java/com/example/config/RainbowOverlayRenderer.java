62d48ef57315fb6e61a88bc3e35249057ab0c065
WalkerTian <903895182@qq.com>
2026-07-30 14:01:44 +0800

    修复ARGB覆盖层被遮挡：提高渲染高度到Y+0.08，使用GL_LEQUAL深度测试

--- src/client/java/com/example/config/RainbowOverlayRenderer.java
+++ src/client/java/com/example/config/RainbowOverlayRenderer.java
@@ -16,6 +16,7 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.chunk.WorldChunk;
 import org.joml.Matrix4f;
+import org.lwjgl.opengl.GL11;
 
 /**
  * 在红石线上绘制动态颜色覆盖层。
@@ -63,6 +64,7 @@ public class RainbowOverlayRenderer {
 
             RenderSystem.enableBlend();
             RenderSystem.defaultBlendFunc();
+            RenderSystem.depthFunc(GL11.GL_LEQUAL);
             RenderSystem.setShader(GameRenderer::getPositionColorProgram);
             RenderSystem.depthMask(false);
 
@@ -87,7 +89,7 @@ public class RainbowOverlayRenderer {
                 float b = (color & 0xFF) / 255f;
 
                 float x = (float)(bx - camPos.x);
-                float y = (float)(by + 0.02 - camPos.y);
+                float y = (float)(by + 0.08 - camPos.y);
                 float z = (float)(bz - camPos.z);
 
                 matrices.push();
